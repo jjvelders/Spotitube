928e105b54864e02c513de7a9df63a08300ca9e9
jevolo <jj.velders@student.han.nl>
2021-10-11 13:35:15 +0200

    Revert "V0.1 First Endpoint"
    This reverts commit 40a239123e77114b1317816c22d663c0638aa6a7.

--- src/main/java/nl/student/data/dao/ITrackDAO.java
+++
@@ -1,9 +0,0 @@
-package nl.student.data.dao;
-
-import nl.student.services.doa.Entity.TrackEnitity;
-
-import java.util.ArrayList;
-
-public interface ITrackDAO {
-    public ArrayList<TrackEnitity> getAllTracks();
-}
